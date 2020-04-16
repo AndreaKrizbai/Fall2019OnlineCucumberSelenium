6d2971ff4665f44044c19218d6eec3f227e99ee9
AndreaKrizbai <krizbai.andrea@gmail.com>
2020-04-15 21:58:23 -0400

    hooks and tags

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -9,7 +9,9 @@ import org.junit.runner.RunWith;
         glue="com/vytrack/step_definitions",
         features = "src/test/resources/features",
         dryRun = false,
-        strict = true
+        strict = true,
+        tags = "@driver",
+        plugin = "html:target/cucumber-html-report"
 )
 public class CucumberRunner {
 }
--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -1,6 +1,7 @@
 package com.vytrack.step_definitions;
 
 import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
@@ -14,7 +15,6 @@ public class LoginStepDefinitions {
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
         System.out.println("Open login page");
-
         String URL = ConfigurationReader.getProperty("qa3");
         Driver.getDriver().get(URL);
     }
@@ -22,7 +22,6 @@ public class LoginStepDefinitions {
     @When("user logs in as a sales manager")
     public void user_logs_in_as_a_sales_manager() {
         System.out.println("Login as sales manager");
-
         loginPage.login("salesmanager110", "UserUser123");
     }
 
@@ -31,12 +30,18 @@ public class LoginStepDefinitions {
         loginPage.login("storemanager85", "UserUser123");
     }
 
+    @When("user logs in as a driver")
+    public void user_logs_in_as_a_driver() {
+        System.out.println("Login as a driver");
+        loginPage.login("user19", "UserUser123");
+    }
 
     @Then("user should verify that title is Dashboard")
     public void user_should_verify_that_title_is_Dashboard() {
         System.out.println("Verify that title is Dashboard");
-
+        BrowserUtils.waitForPageToLoad(10);
+        BrowserUtils.wait(2);
         Assert.assertEquals("Dashboard", Driver.getDriver().getTitle());
-        Driver.closeDriver();
     }
+
 }
